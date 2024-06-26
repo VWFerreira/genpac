be4c773e21a35530c2728570afa5c8d6c6527b6c
VWFerreira <98263784+VWFerreira@users.noreply.github.com>
2024-06-26 15:59:45 -0300

    contrato.py

--- contrato.py
+++ contrato.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from datetime import datetime
+from pytz import timezone
 
-# Configurar o layout para largura total
-st.set_page_config(layout="wide")
+# Configurar o layout
+st.set_page_config(layout="wide", page_title="Relatório Banrisul")
 
 # Carregar o arquivo CSS
 with open("./css/app.css") as f:
@@ -12,7 +13,7 @@ with open("./css/app.css") as f:
 from saldos import main as mostrar_saldos
 from Valor_contrato import principal as mostrar_valor
 from relatoriodiario import relatoriodiario as mostrar_relatorio
-from gerar_contrato import  gerar_contrato as gerar_contrato
+from gerar_contrato import gerar_contrato as gerar_contrato
 
 PAGES = {
     "Contrato Geral": gerar_contrato,
@@ -21,9 +22,11 @@ PAGES = {
     "Saldos Contratos": mostrar_saldos,
 }
 
-# Adicionar cabeçalho com data, hora e dia da semana lado a lado
-st.markdown('<div class="header-container">', unsafe_allow_html=True)
-now = datetime.now()
+# Título do Relatório
+st.markdown("<h1 style='text-align: center; font-size: 48px;'>Relatório Banrisul</h1>", unsafe_allow_html=True)
+
+# Data e Hora de Brasília
+now = datetime.now(timezone('America/Sao_Paulo'))
 current_time = now.strftime("%H:%M:%S")
 current_date = now.strftime("%Y-%m-%d")
 current_day = now.strftime("%A")
@@ -38,20 +41,16 @@ st.markdown(
     ''', 
     unsafe_allow_html=True,
 )
-st.markdown('</div>', unsafe_allow_html=True)
 
-# Criar a barra lateral
 st.sidebar.image("./image/logo.png", use_column_width=True)
 st.sidebar.markdown("# Banrisul")
 
 # Adicionar botões de navegação na barra lateral
 selected_page = st.sidebar.radio("Selecione a página", list(PAGES.keys()))
 
-# Função para mostrar as páginas
-
-# Carregar a página selecionada
 if selected_page:
     page = PAGES[selected_page]
     page()
 else:
     st.markdown('<div class="stAlert">Selecione uma página no menu acima.</div>', unsafe_allow_html=True)
+
